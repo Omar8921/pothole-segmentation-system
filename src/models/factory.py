92f5c3ca1f1980706e37ea7bdec99a7c113298f1
Omar8921 <omaralmadani05@gmail.com>
2026-09-14 11:29:03 -0400

    Fix typo in build_segformer method

--- src/models/factory.py
+++ src/models/factory.py
@@ -7,7 +7,7 @@ def get_model(model_name: str, num_classes: int):
         return build_unet(num_classes=num_classes)
 
     elif model_name.lower() == 'segformer':
-        return build_segformer(num_classse=num_classes)
+        return build_segformer(num_classes=num_classes)
 
     elif model_name.lower() == 'deeplabv3':
         return build_deeplabv3(num_classes=num_classes)
--- src/models/segformer.py
+++ src/models/segformer.py
@@ -1,7 +1,7 @@
 from segmentation_models_pytorch import Segformer
 
-def build_segformer(num_classse: int):
+def build_segformer(num_classes: int):
     return Segformer(
         encoder_name='resnet34',
-        num_classse=num_classse
+        num_classse=num_classes
     )
